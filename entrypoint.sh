ad457586da55aa2f66a90ce773ce8dcdb131b0e8
mayartmal <mayartmal@gmail.com>
2025-06-30 16:43:36 +0400

    add dockerfile, entrypoint adn gitignore

---
+++ entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+echo "Running JMeter test"
+jmeter -n -t /qpp-test-plan.jmx -l /result.jtl -j /jmeter.log
+echo "JMeter Test Finished"
+
+
+if [ -d /results ]; then
+    echo "copy results to /results"
+    cp /result.jtl /results/
+    cp /jmeter.log /results/
+else
+    echo "/results not found. skip this stage"
+fi
+
+echo "bash script finished"
+
+# tail -f /dev/null
